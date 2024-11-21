3344a4305a0f5384a2e990dc3c10e24e9f9c9022
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2024-11-21 22:01:45 +0100

    Create file

---
+++ 392-IsSubsequence/392-IsSubsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int left = 0, right = 0;
+        while ((left < s.length()) && (right < t.length())) {
+            if (s.charAt(left)== t.charAt(right)) {
+                left++;
+                right++;
+            } else {
+                right++;
+            }
+        }
+        if (left >= s.length()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
