3eb5001b9678c4ef926d1654a546ec9158d87331
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2024-11-26 17:57:53 +0100

    Create file

---
+++ 2460-ApplyOperationstoanArray/2460-ApplyOperationstoanArray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int left = 0;
+        int[] res = new int[nums.length];
+        int k=0;
+        for (int right =1;right<nums.length;right++) {
+            if (nums[left] == nums[right]) {
+                res[k] = nums[left] * 2;
+                nums[right] = 0;
+                left++;
+                k++;
+            } else {
+                res[k] = nums[left];
+                k++;
+                left++;
+            }
+        }
+        res[k] = nums[left];
+        int j=0;
+        for (int i=0;i< res.length;i++) {
+            if (res[i] != 0) {
+                res[j] = res[i];
+                j++;
+            }
+        }
+        for (int i=j;i<res.length;i++) {
+            res[i] = 0;
+        }
+        return res;
+    }
+}
