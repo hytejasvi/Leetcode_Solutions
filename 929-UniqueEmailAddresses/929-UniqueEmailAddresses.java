4e8b47e091e47010698534802402b7138997d6e2
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2024-12-07 23:19:43 +0100

    Create file

---
+++ 929-UniqueEmailAddresses/929-UniqueEmailAddresses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmails = new HashSet<>();
+        
+        for (String email : emails) {
+            // Split the email into local and domain parts
+            String[] parts = email.split("@");
+            String local = parts[0];
+            String domain = parts[1];
+            
+            // Process the local part: remove everything after '+' and all '.'
+            if (local.contains("+")) {
+                local = local.substring(0, local.indexOf('+'));
+            }
+            local = local.replace(".", "");
+            
+            // Combine the processed local part with the domain
+            String normalizedEmail = local + "@" + domain;
+            
+            // Add the normalized email to the set
+            uniqueEmails.add(normalizedEmail);
+        }
+        
+        // The size of the set is the number of unique emails
+        return uniqueEmails.size();
+    }
+}
