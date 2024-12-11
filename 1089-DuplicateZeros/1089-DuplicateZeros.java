9e01206cb5085c133b05f5fb3a977071bd9ddcd5
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2024-12-11 21:37:24 +0100

    Update 1089-DuplicateZeros.java

--- 1089-DuplicateZeros/1089-DuplicateZeros.java
+++ 1089-DuplicateZeros/1089-DuplicateZeros.java
@@ -2,6 +2,7 @@ class Solution {
     public void duplicateZeros(int[] arr) {
         int[] res = new int[arr.length];
         int i=0,k=0;
+
         while(k<res.length) {
             if (arr[i] != 0) {
                 res[k] = arr[i];
@@ -13,6 +14,7 @@ class Solution {
                 k +=2;
             }
         }
+
         System.arraycopy(res, 0, arr, 0, res.length);
     }
 }
