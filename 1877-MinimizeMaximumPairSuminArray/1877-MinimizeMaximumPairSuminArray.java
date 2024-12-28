7984fa412458b9a540dbc73db15d3937e0a5b829
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2024-12-28 15:52:49 +0100

    Create file

---
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length-1;
+        int sum = 0;
+        while (left < right) {
+            sum = Math.max(sum, (nums[left]+ nums[right]));
+            left++;
+            right--;
+        }
+        return sum;
+    }
+}
