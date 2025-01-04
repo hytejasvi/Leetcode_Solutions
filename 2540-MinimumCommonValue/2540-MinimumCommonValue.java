e8e6ad45cd3178ad6ddd8f20cdeccbec27820ee2
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-04 15:48:30 +0100

    Update file 1/4/2025, 3:48:30 PM

--- 2540-MinimumCommonValue/2540-MinimumCommonValue.java
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.java
@@ -1,6 +1,9 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int left = 0, right = 0;
+        if((nums1[left] > nums2[nums2.length-1]) || (nums2[right] > nums1[nums1.length-1])) {
+            return -1;
+        }
         while (left < nums1.length && right < nums2.length) {
             if(nums1[left] < nums2[right]) {
                 left++;
