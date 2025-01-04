22efd565c273373ac730f791d266318afd5bfb3f
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-04 12:38:49 +0100

    Update file 1/4/2025, 12:38:49 PM

--- 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
@@ -1,6 +1,6 @@
 class Solution {
-    public String reverseWords(String s) {
-        /*if(s.length() == 1) {
+    /*public String reverseWords(String s) {
+        if(s.length() == 1) {
             return s;
         }
         StringBuilder sb = new StringBuilder(s.length());
@@ -13,13 +13,22 @@ class Solution {
             }
             sb.append(" ");
         }
-        return sb.toString().trim();*/
+        return sb.toString().trim();
+    }*/
 
+//Approach 2, with better performance
+/*    public String reverseWords(String s) {
         String[] words = s.split("\\s+");
         StringBuilder result = new StringBuilder(s.length());
         for(String word : words) {
             result.append(new StringBuilder(word).reverse().append(" "));
         }
         return result.toString().trim();
-    }
+    } */
+
+     public String reverseWords(String s) {
+        return Arrays.stream(s.split(" "))
+                     .map(word -> new StringBuilder(word).reverse().toString())
+                     .collect(Collectors.joining(" "));
+     }
 }
