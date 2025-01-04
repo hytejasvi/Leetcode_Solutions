b5e4b1f16d8ee326157c6219686ce6b7c0fca499
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-04 16:21:09 +0100

    Update file 1/4/2025, 4:21:10 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,38 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        /*if (nums.length == 1) {
-            return ;
-        } 
-        int left = 0, right=1;
-        while (right < nums.length) {
-            if (nums[left] == 0 && nums[right] != 0) {
-                nums[left] = nums[right];
-                nums[right] = 0;
-                left++;
-                right++;
-            } else if (nums[left] == 0 && nums[right] == 0) {
-                right++;
-            } else if (nums[left] != 0 && nums[right] == 0) {
-                left++;
-                right++;
-            } else if (nums[left] != 0 && nums[right] != 0) {
-                left++;
-                right++;
+        // keep track of the position of "0"
+        int pos = 0; 
+        for(int i = 0; i < nums.length; i++) {
+            // if the current element is not "0", swap it with the element at position "pos"
+            if(nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[pos];
+                nums[pos] = temp;
+                // increment the position of "0"
+                pos++;
             }
-        }*/
-        int left = 0; // Points to the position where the next non-zero element should be placed
-
-    // Move non-zero elements to the front
-    for (int right = 0; right < nums.length; right++) {
-        if (nums[right] != 0) {
-            nums[left] = nums[right];
-            left++;
         }
-    }
-
-    // Fill the remaining positions with zeros
-    for (int i = left; i < nums.length; i++) {
-        nums[i] = 0;
-    }
     }
 }
