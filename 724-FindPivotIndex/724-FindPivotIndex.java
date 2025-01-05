64d8dfaabb8fdba7d739d7edd3893be042af5f4f
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-05 21:59:12 +0100

    Update file 1/5/2025, 9:59:12 PM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,24 +1,22 @@
 class Solution {
-    
-  int sum(int[] numbers) {
-    int sum = 0;
-    for (int i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        int[] sumArray = new int[nums.length];
+        for(int i=0;i<nums.length;i++) {
+            sum = sum+nums[i];
+            sumArray[i] = sum;
+        }
+        int start = 0;
+        int res = -1;
+        int end = nums.length-1;
+        for(int right=0;right<nums.length;right++) {
+            if((sumArray[end] - sumArray[start]) == 0) {
+                return 0;
+            }
+            if(sumArray[end]-sumArray[right] == sumArray[right] - nums[right]) {
+                return right;
+            }
+        }
+        return res;
     }
-    return sum;
-}
-
-int pivotIndex(int[] nums) {
-  int leftSum = 0;
-  int arraySum = sum(nums);
-
-  for (int i = 0; i < nums.length; ++i) {
-    if (leftSum == arraySum - leftSum - nums[i]) {
-      return i;
-    }
-    leftSum += nums[i];
-  }
-
-  return -1;
-}
 }
