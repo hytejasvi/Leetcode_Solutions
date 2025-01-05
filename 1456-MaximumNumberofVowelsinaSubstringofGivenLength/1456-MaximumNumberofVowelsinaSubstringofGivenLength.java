1bb63a37781945ebf565b36c73799a65d878d819
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-05 18:48:28 +0100

    Update file 1/5/2025, 6:48:28 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.java
@@ -1,17 +1,21 @@
 class Solution {
     public int maxVowels(String s, int k) {
+        Set<Character> charSet = new HashSet<>();
+        charSet.add('a');
+        charSet.add('e');
+        charSet.add('i');
+        charSet.add('o');
+        charSet.add('u');
         int maxCount = 0;
         int windowCount = 0;
         int left = 0;
         for(int right=0;right<s.length();right++) {
-            if(s.charAt(right) == 'a' | s.charAt(right) == 'e' | s.charAt(right) == 'i' |
-                s.charAt(right) == 'o' | s.charAt(right) == 'u') {
-                    windowCount++;
+            if(charSet.contains(s.charAt(right))) {
+                windowCount++;
             }
             while(right-left+1 > k) {
-                if(s.charAt(left) == 'a' | s.charAt(left) == 'e' | s.charAt(left) == 'i' |
-                     s.charAt(left) == 'o' | s.charAt(left) == 'u') {
-                windowCount--;
+                if(charSet.contains(s.charAt(left))) {
+                    windowCount--;
                 }
                 left++;
             }
