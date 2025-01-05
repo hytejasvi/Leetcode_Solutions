c1855275c3431a664e851f5febfebf2318fea1fc
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-05 22:13:29 +0100

    Update file 1/5/2025, 10:13:29 PM

--- 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.java
@@ -1,16 +1,20 @@
 class NumArray {
+    int[] sumArray ;
     int[] nums;
     public NumArray(int[] nums) {
         this.nums = nums;
+        this.sumArray = new int[nums.length];
+        int sum=0;
+        for(int i=0;i<nums.length;i++) {
+            sum = sum+nums[i];
+            sumArray[i] = sum;
+        }
     }
     
     public int sumRange(int left, int right) {
-        int sum=0;
-        while(left <= right) {
-            sum = sum+nums[left];
-            left++;
-        }
-        return sum;
+        int res=0;
+        res = sumArray[right] - sumArray[left]+nums[left];
+        return res;
     }
 }
 
