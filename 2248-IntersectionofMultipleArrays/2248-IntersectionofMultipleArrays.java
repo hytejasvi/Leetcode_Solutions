8b733c0973a9b4de94793d020e227957b6d252ad
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-07 22:36:12 +0100

    Update file 1/7/2025, 10:36:14 PM

--- 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.java
+++ 2248-IntersectionofMultipleArrays/2248-IntersectionofMultipleArrays.java
@@ -2,13 +2,13 @@ class Solution {
     public List<Integer> intersection(int[][] nums) {
         Map<Integer, Integer> myMap = new HashMap<>();
         List<Integer> res = new ArrayList<>();
-        for(int[] n : nums) {
-            for(int m: n) {
-                myMap.put(m, myMap.getOrDefault(m, 0)+1);
+        for (int[] n : nums) {
+            for (int m : n) {
+                myMap.put(m, myMap.getOrDefault(m, 0) + 1);
             }
         }
-        for(Map.Entry<Integer, Integer> entry: myMap.entrySet()) {
-            if(entry.getValue() == nums.length) {
+        for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
+            if (entry.getValue() == nums.length) {
                 res.add(entry.getKey());
             }
         }
