017b292912023e9fec226132cfe933895f631616
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-21 12:33:28 +0100

    Update file 1/21/2025, 12:33:28 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,10 +1,11 @@
 class Solution {
     public int maxArea(int[] height) {
         int left = 0, right = height.length-1;
-        int maxWater = 0;
+        int maxWater = Integer.MIN_VALUE;
         while(left < right) {
-            //int currentWater = (right-left) * (Math.min(height[left], height[right]));
-            maxWater = Math.max(maxWater, ((right-left) * (Math.min(height[left], height[right]))));
+            final int minHeight = Math.min(height[left], height[right]);
+            final int length = right - left;
+            maxWater = Math.max(maxWater, (minHeight*length));
             if(height[left] <= height[right]) {
                 left++;
             } else {
