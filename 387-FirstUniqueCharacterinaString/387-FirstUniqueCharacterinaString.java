f095220a5d808f62784c35b5d2793b17f2b7517b
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-27 12:58:21 +0100

    Update file 1/27/2025, 12:58:21 PM

--- 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.java
@@ -1,22 +1,13 @@
 class Solution {
     public int firstUniqChar(String s) {
         int uniqueCharIndex = -1;
-        char[] charArray = s.toCharArray();
-        System.out.println(Arrays.toString(charArray));
-        for(int i=0;i< charArray.length;i++) {
-            char c = charArray[i];
-            boolean isUniqueChar = true;
-            if(c != '0') {
-                for (int j = i + 1; j < charArray.length; j++) {
-                    if (charArray[j] == c) {
-                        charArray[j] = '0';
-                        isUniqueChar = false;
-                    }
-                }
-                if (isUniqueChar) {
-                    uniqueCharIndex = i;
-                    return uniqueCharIndex;
-                }
+        int[] frequencyCount = new int[26];
+        for(char c: s.toCharArray()) {
+            frequencyCount[c-'a']++;
+        }
+        for(int i=0;i< s.length();i++) {
+            if(frequencyCount[s.charAt(i)-'a'] == 1) {
+                return i;
             }
         }
         return uniqueCharIndex;
