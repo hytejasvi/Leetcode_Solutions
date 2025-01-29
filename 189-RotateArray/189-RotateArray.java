8d35c49156431ab77b2ccbab18816686a355cd6b
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-01-29 10:34:05 +0100

    Create file

---
+++ 189-RotateArray/189-RotateArray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(k>0) {
+            k = k%nums.length;
+            int left = 0, right = nums.length-1;
+            while(left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+            left = 0; right = k-1;
+            while(left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+            left=k; right = nums.length-1;
+            while(left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
