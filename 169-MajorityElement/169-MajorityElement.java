28e4001a1c8866d4a7963aeffa9a239c572d8a85
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-09-10 23:27:49 +0200

    File updated at 9/10/2025, 11:27:49 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,13 +1,13 @@
-// Last updated: 9/10/2025, 11:20:26 PM
+// Last updated: 9/10/2025, 11:27:49 PM
 class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> myMap = new HashMap<>();
         int n = nums.length/2;        
         for(int num: nums) {
             myMap.put(num, myMap.getOrDefault(num, 0)+1);
-            if(myMap.containsValue(n+1)) {
-                return num;
-            }
+            if (myMap.get(num) > n) {
+            return num;
+        }
         }
         return n;
     }
