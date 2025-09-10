07b09f7744b6bc070c374820813d1e19b177ddbb
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-09-10 11:41:38 +0200

    File updated at 9/10/2025, 11:41:40 AM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,26 +1,26 @@
-// Last updated: 9/9/2025, 10:20:01 PM
+// Last updated: 9/10/2025, 11:41:40 AM
 class Solution {
+    
     public void sortColors(int[] nums) {
-        int red=0;
-        int white=0;
-        int blue=0;
-        for(int n: nums) {
-            if(n==0) {
-                red++;
-            } else if(n==1) {
-                white++;
-            } else{
-                blue++;
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while (mid<=high){
+            if (nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            } else if (nums[mid]==1){
+                mid++;
+            }else if (nums[mid]==2){
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
             }
         }
-        for(int i= 0;i<red;i++) {
-            nums[i] = 0;
-        }
-        for(int i= red;i<=(red+white-1);i++) {
-            nums[i] = 1;
-        }
-        for(int i= red+white;i<=(red+white+blue-1);i++) {
-            nums[i] = 2;
-        }
+        //for (int k=0;k<nums.length;k++) System.out.print(nums[k]);
     }
 }
