dad31a44f9d6f905e2291c56fbe1084f01123365
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-09-12 22:33:23 +0200

    File updated at 9/12/2025, 10:33:23 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,7 +1,7 @@
-// Last updated: 9/10/2025, 11:27:49 PM
+// Last updated: 9/12/2025, 10:33:23 PM
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> myMap = new HashMap<>();
+        /*Map<Integer, Integer> myMap = new HashMap<>();
         int n = nums.length/2;        
         for(int num: nums) {
             myMap.put(num, myMap.getOrDefault(num, 0)+1);
@@ -9,6 +9,23 @@ class Solution {
             return num;
         }
         }
-        return n;
+        return n;*/
+
+        // a single pass with no extra memory:
+        int num = 0;
+        int count = 0;
+        int n = nums.length / 2;
+        for(int a: nums) {
+            if(count == 0) {
+                num = a;
+            }
+            //count += num==a ? 1: -1; short way of if-else
+            if(num ==a) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return num;
     }
 }
