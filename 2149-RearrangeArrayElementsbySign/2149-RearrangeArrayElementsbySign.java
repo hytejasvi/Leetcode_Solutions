d6f9027fbf3a0b5b0f441e57b90a285dc63e1b83
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-09-13 13:01:30 +0200

    File updated at 9/13/2025, 1:01:30 PM

--- 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.java
+++ 2149-RearrangeArrayElementsbySign/2149-RearrangeArrayElementsbySign.java
@@ -1,15 +1,30 @@
+// Last updated: 9/13/2025, 1:01:30 PM
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        int[] posArr = new int[nums.length/2];
+        int[] negArr = new int[nums.length/2];
         int[] res = new int[nums.length];
-        int left = 0, right = left+1;
-        for(int i=0;i< nums.length;i++) {
-            if(nums[i] >0) {
-                res[left] = nums[i];
-                left +=2;
+        int j=0;
+        int pos=0;
+        int neg=0;
+        for(int n: nums) {
+            if(n > 0) {
+                posArr[pos] = n;
+                pos++;
             } else {
-                res[right] = nums[i];
-                right += 2;
+                negArr[neg] = n;
+                neg++;
             }
+        }
+        int k=0;
+        for(int i: posArr) {
+            res[k] = i;
+            k +=2;
+        }
+        k=1;
+        for(int i: negArr) {
+            res[k] = i;
+            k +=2;
         }
         return res;
     }
