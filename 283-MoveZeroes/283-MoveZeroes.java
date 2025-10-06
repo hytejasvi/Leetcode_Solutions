118ac243594d15415847d52fd04dbdddb07f05d5
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-10-06 09:47:51 +0200

    File updated at 10/6/2025, 9:47:52 AM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,10 +1,12 @@
-// Last updated: 9/8/2025, 12:30:05 PM
+// Last updated: 10/6/2025, 9:47:52 AM
 class Solution {
     public void moveZeroes(int[] nums) {
         if(nums.length ==1) {
             return;
         }
-        int left = 0;
+        //approach 1: simply move all non zero elements to the front and keep incrementing the left pointer.
+        //then iterate with left till array size and put 0 
+        /*int left = 0;
         int right = 0;
         for(int i=0;i< nums.length;i++) {
             if(nums[i] != 0) {
@@ -15,6 +17,16 @@ class Solution {
         while(left < nums.length) {
             nums[left] = 0;
             left++;
+        }*/
+        int left = 0, right =0;
+        while(right < nums.length) {
+            if(nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = 0;
+                nums[left] = temp;
+                left++;
+            }
+            right++;
         }
     }
 }
