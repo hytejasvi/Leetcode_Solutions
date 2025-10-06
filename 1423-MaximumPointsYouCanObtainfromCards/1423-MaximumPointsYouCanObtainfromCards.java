e6f2ad674e0f0ccf5d567d8a331718957e3fa872
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-10-06 22:19:41 +0200

    File updated at 10/6/2025, 10:19:41 PM

--- 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.java
+++ 1423-MaximumPointsYouCanObtainfromCards/1423-MaximumPointsYouCanObtainfromCards.java
@@ -1,4 +1,4 @@
-// Last updated: 10/6/2025, 10:07:38 PM
+// Last updated: 10/6/2025, 10:19:41 PM
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n = cardPoints.length;
@@ -13,9 +13,11 @@ class Solution {
             return totalSum;
         }
         int left = 0, windowSum =0, maxScore = 0;
+        int windowSize = n - k; //the required window
+        //i-left+1  --> current window size
         for(int i=0;i< n;i++) {
             windowSum += cardPoints[i];
-            if((n-k) == (i-left+1)/*window size*/) {
+            if((i-left+1) == windowSize) {
                 maxScore = Math.max(maxScore, totalSum - windowSum);
                 windowSum = windowSum - cardPoints[left];
                 left++;
