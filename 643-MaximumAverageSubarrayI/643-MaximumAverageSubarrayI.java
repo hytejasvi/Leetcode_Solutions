8aac63944ff9ef95e372cd3482626a0784a0784f
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-10-13 13:24:45 +0200

    File updated at 10/13/2025, 1:24:46 PM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -1,24 +1,31 @@
-// Last updated: 10/13/2025, 12:18:13 PM
+// Last updated: 10/13/2025, 1:24:46 PM
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        double sum=0;
-        for (int i=0;i<k;i++){
-            sum+=nums[i];
+        /*double maxAvg = -Double.MAX_VALUE;
+        double windowSum = 0.0;
+        for(int i=0;i<k;i++) {
+            windowSum = windowSum+nums[i];
         }
-        double ans=-Double.MAX_VALUE;
-        double avg=(sum)/(double)k;
-        ans=Math.max(ans,avg);
-        int i=0;
-        int j=k;
-        while(j<nums.length){
-            sum+=nums[j];
-            sum-=nums[i];
-            avg=(sum)/(double)k;
-            ans=Math.max(ans,avg);
-            i++;
-            j++;
+        maxAvg = Math.max(maxAvg, windowSum/(double)k);
+        int left=0;
+        for(int i=k;i< nums.length;i++) {
+            windowSum = windowSum + nums[i] - nums[left];
+            maxAvg = Math.max(maxAvg, windowSum/(double)k);
+            left++;
         }
-        return ans;
-        
+        return maxAvg; */
+
+        int windowSum = 0;
+        for(int i=0;i<k;i++) {
+            windowSum = windowSum+nums[i];
+        }
+        int maxSum = windowSum;
+        int left = 0;
+        for(int i=k;i< nums.length;i++) {
+            windowSum = windowSum+nums[i]-nums[left];
+            maxSum = Math.max(maxSum, windowSum);
+            left++;
+        }
+        return maxSum/(double)k;
     }
 }
