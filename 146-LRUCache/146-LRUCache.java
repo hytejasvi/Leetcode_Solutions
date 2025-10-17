b39d9c8c2dc7ec8492c4dae4ce2e62a7466b077d
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-10-17 18:48:39 +0200

    Create 146-LRUCache

---
+++ 146-LRUCache/146-LRUCache.java
@@ -0,0 +1,71 @@
+// Last updated: 10/17/2025, 6:48:40 PM
+class LRUCache {
+    private class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+    private final Node head, tail; 
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+         map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insertToHead(node);
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            insertToHead(node);
+        } else {
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node newNode = new Node(key, value);
+            insertToHead(newNode);
+            map.put(key, newNode);
+        }
+    }
+
+    // helper: remove a node from list
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // helper: insert node right after head
+    private void insertToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
