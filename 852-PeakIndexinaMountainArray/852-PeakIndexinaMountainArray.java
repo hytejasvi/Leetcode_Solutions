6e08963146f4acc61c2461dde3bf44651b9a1e04
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-11-01 19:16:41 +0100

    File updated at 11/1/2025, 7:16:42 PM

--- 852-PeakIndexinaMountainArray/852-PeakIndexinaMountainArray.java
+++ 852-PeakIndexinaMountainArray/852-PeakIndexinaMountainArray.java
@@ -1,26 +1,15 @@
-// Last updated: 11/1/2025, 7:10:07 PM
+// Last updated: 11/1/2025, 7:16:42 PM
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        if (arr[0] > arr[1]) {
-            return 0;
-        } else if (arr[arr.length-1] > arr[arr.length-2]) {
-            return arr.length-1;
-        }
-        int start = 1, end = arr.length-2;
-        int ans = 0;
-        while (start <= end) {
-            int mid = start + (end-start)/2;
-            if (arr[mid] > arr[mid-1]) {
-                if (arr[mid] > arr[mid+1]) {
-                    ans =  mid;
-                    start = mid+1;
-                } else {
-                    start = mid+1;
-                }
-            } else if (arr[mid] < arr[mid-1]) {
-                end = mid-1;
+        int left = 0, right = arr.length-1;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            if(arr[mid] < arr[mid+1]) {
+                left = mid+1;
+            } else {
+                right = mid-1;
             }
         }
-        return ans;
+        return left;
     }
 }
