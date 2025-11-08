ba3027862823dbbca00767870964ff0de646e61e
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-11-08 19:52:26 +0100

    File updated at 11/8/2025, 7:52:25 PM

--- 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
@@ -1,34 +1,12 @@
+// Last updated: 11/8/2025, 7:52:25 PM
 class Solution {
-    /*public String reverseWords(String s) {
-        if(s.length() == 1) {
-            return s;
+    public String reverseWords(String s) {
+        String[] s1 = s.split(" ");
+        StringBuilder res = new StringBuilder();
+        for(String s2: s1) {
+            StringBuilder sb = new StringBuilder(s2);
+            res.append(sb.reverse()).append(" ");
         }
-        StringBuilder sb = new StringBuilder(s.length());
-        String[] strArray = s.split("\\s+");
-        for(String str : strArray) {
-            int right = str.length() - 1;
-            while(right >=0) {
-                sb.append(str.charAt(right));
-                right--;
-            }
-            sb.append(" ");
-        }
-        return sb.toString().trim();
-    }*/
-
-//Approach 2, with better performance
-/*    public String reverseWords(String s) {
-        String[] words = s.split("\\s+");
-        StringBuilder result = new StringBuilder(s.length());
-        for(String word : words) {
-            result.append(new StringBuilder(word).reverse().append(" "));
-        }
-        return result.toString().trim();
-    } */
-
-     public String reverseWords(String s) {
-        return Arrays.stream(s.split(" "))
-                     .map(word -> new StringBuilder(word).reverse().toString())
-                     .collect(Collectors.joining(" "));
-     }
+        return res.toString().trim();
+    }
 }
