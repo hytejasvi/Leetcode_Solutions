ae5d099c550d310369023f1f63ebce7d549fde01
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-11-11 18:22:44 +0100

    File updated at 11/11/2025, 6:22:44 PM

--- 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
+++ 2161-PartitionArrayAccordingtoGivenPivot/2161-PartitionArrayAccordingtoGivenPivot.java
@@ -1,36 +1,28 @@
-// Last updated: 11/11/2025, 6:20:08 PM
+// Last updated: 11/11/2025, 6:22:44 PM
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int[] res = new int[nums.length];
-        List<Integer> low = new ArrayList<>();
-        List<Integer> mid = new ArrayList<>();
-        List<Integer> high = new ArrayList<>();
-        for(int i: nums) {
-            if(i<pivot) {
-                low.add(i);
-            } else if(i > pivot) {
-                high.add(i);
-            } else {
-                mid.add(i);
+        int n = nums.length;
+        int[] ans = new int[n];
+        int index = 0;
+
+        for (int num : nums) {
+            if (num < pivot) {
+                ans[index++] = num;
             }
         }
-        int i=0;
-        while(i<low.size()) {
-            res[i] = low.get(i);
-            i++;
+
+        for (int num : nums) {
+            if (num == pivot) {
+                ans[index++] = num;
+            }
         }
-        int j=0;
-        while(j<mid.size()) {
-            res[i] = mid.get(j);
-            i++;
-            j++;
+
+        for (int num : nums) {
+            if (num > pivot) {
+                ans[index++] = num;
+            }
         }
-        int k=0;
-        while(k<high.size()) {
-            res[i] = high.get(k);
-            i++;
-            k++;
-        }
-        return res;
+
+        return ans;
     }
 }
