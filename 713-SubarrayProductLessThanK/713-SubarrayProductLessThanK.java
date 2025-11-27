d1f249f1b979baca314272e1531508bc4dba13cd
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-11-27 21:49:34 +0100

    File updated at 11/27/2025, 9:49:34 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -1,17 +1,19 @@
-class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) {
-            return 0;
-        }
-        int left = 0, ans=0, wp=1;
-        for (int right = 0; right <nums.length;right++) {
-            wp *= nums[right];
-            while(wp >= k) {
-                wp = wp/nums[left];
-                left++;
-            }
-            ans += right-left+1;
-        }
-        return ans;
-    }
-}
+// Last updated: 11/27/2025, 9:49:34 PM
+1class Solution {
+2    public int numSubarrayProductLessThanK(int[] nums, int k) {
+3        if(k == 0) {
+4            return 0;
+5        }
+6        int prod = 1, left = 0;
+7        int count = 0;
+8        for(int right = 0; right < nums.length;right++) {
+9            prod = prod*nums[right];
+10            while(left <= right && prod >= k) {
+11                prod = prod/nums[left];
+12                left++;
+13            }
+14            count = count + (right - left+1);
+15        }
+16        return count;
+17    }
+18}
