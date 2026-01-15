76faf1c98a7c85567260886b84925dfa0f27c956
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-15 20:19:47 +0100

    File updated at 1/15/2026, 8:19:47 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,24 +1,39 @@
-// Last updated: 11/12/2025, 3:01:05 PM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) {
-            return false;
-        }
-        int[] sArray = new int[26];
-        for(char c: s.toCharArray()) {
-            sArray[c - 'a']++;
-        }
-        for(char c: t.toCharArray()) {
-            sArray[c-'a']--;
-            if(sArray[c-'a'] < 0) {
-                return false;
-            }
-        }
-        for (int i: sArray) {
-            if(i != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
+// Last updated: 1/15/2026, 8:19:47 PM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3       /* if(s.length() != t.length()) {
+4            return false;
+5        }
+6        int[] sArray = new int[26];
+7        for(char c: s.toCharArray()) {
+8            sArray[c - 'a']++;
+9        }
+10        for(char c: t.toCharArray()) {
+11            sArray[c-'a']--;
+12            if(sArray[c-'a'] < 0) {
+13                return false;
+14            }
+15        }
+16        for (int i: sArray) {
+17            if(i != 0) {
+18                return false;
+19            }
+20        }
+21        return true;*/
+22        //best approach:
+23        if(s.length() != t.length()) return false;
+24    
+25    int[] count = new int[26];
+26    
+27    for(int i = 0; i < s.length(); i++) {
+28        count[s.charAt(i) - 'a']++;
+29        count[t.charAt(i) - 'a']--;
+30    }
+31    
+32    for(int i : count) {
+33        if(i != 0) return false;
+34    }
+35    
+36    return true;
+37    }
+38}
