2adb29f8d79ec4ffe0924208c1d4830e0f7ea172
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-16 21:07:13 +0100

    File updated at 1/16/2026, 9:07:13 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.java
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.java
@@ -1,25 +1,35 @@
-// Last updated: 11/24/2025, 10:15:28 PM
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        if(nums.length < 3) {
-            return nums.length;
-        }
-        int left = 0, right = 0;
-        
-        while(right < nums.length) {
-            int count=1;
-            while(right+1 < nums.length && nums[right] == nums[right+1]) {
-                count++;
-                right++;
-            }
-            int swaps = Math.min(count, 2);
-            while(swaps > 0) {
-                nums[left] = nums[right];
-                left++;
-                swaps--;
-            }
-            right++;
-        }
-        return left;
-    }
-}
+// Last updated: 1/16/2026, 9:07:13 PM
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        if(nums.length < 3) {
+4            return nums.length;
+5        }
+6       /* int left = 0, right = 0;
+7        
+8        while(right < nums.length) {
+9            int count=1;
+10            while(right+1 < nums.length && nums[right] == nums[right+1]) {
+11                count++;
+12                right++;
+13            }
+14            int swaps = Math.min(count, 2);
+15            while(swaps > 0) {
+16                nums[left] = nums[right];
+17                left++;
+18                swaps--;
+19            }
+20            right++;
+21        }
+22        return left;*/
+23
+24        //a more simpler and generic approach for maximum k duplicates:
+25        int i = 0;
+26        for(int num: nums) {
+27            if(i < 2 || nums[i-2] != num) {
+28                nums[i] = num;
+29                i++;
+30            }
+31        }
+32        return i;
+33    }
+34}
