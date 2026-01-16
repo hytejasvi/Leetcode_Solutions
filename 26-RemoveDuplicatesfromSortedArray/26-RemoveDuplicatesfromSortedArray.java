b89b6c5cdf762e9549ecbfd140d8fe29273f05ed
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-16 20:44:27 +0100

    File updated at 1/16/2026, 8:44:27 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,27 +1,27 @@
-// Last updated: 9/25/2025, 9:15:10 AM
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        if(nums.length == 1) {
-            return 1;
-        }
-        /*int left = 0;
-        int right = 1;
-        while(right < nums.length) {
-            if(nums[left] == nums[right]) {
-                right ++;
-            } else {
-                nums[++left] = nums[right];
-                right++;
-            }
-        }
-        return left+1;*/
-
-        int left = 0;
-        for(int i=1;i< nums.length;i++) {
-            if(nums[left] != nums[i]) {
-                nums[++left] = nums[i];
-            }
-        }
-        return left+1;
-    }
-}
+// Last updated: 1/16/2026, 8:44:27 PM
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        if(nums.length == 1) {
+4            return 1;
+5        }
+6        /*int left = 0;
+7        int right = 1;
+8        while(right < nums.length) {
+9            if(nums[left] == nums[right]) {
+10                right ++;
+11            } else {
+12                nums[++left] = nums[right];
+13                right++;
+14            }
+15        }
+16        return left+1;*/
+17
+18        int left = 0;
+19        for(int i=1;i< nums.length;i++) {
+20            if(nums[left] != nums[i]) {
+21                nums[++left] = nums[i];
+22            }
+23        }
+24        return left+1;
+25    }
+26}
