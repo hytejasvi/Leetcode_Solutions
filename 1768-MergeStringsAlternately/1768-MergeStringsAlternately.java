746f6d440f7e88c96f6e57b34d61278f1f94a213
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-17 15:57:37 +0100

    File updated at 1/17/2026, 3:57:38 PM

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -1,22 +1,22 @@
-// Last updated: 9/24/2025, 10:28:54 PM
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        StringBuilder merged = new StringBuilder();
-        int i = 0;
-        int j = 0;
-
-        while(i < word1.length() && j < word2.length()){
-            merged.append(word1.charAt(i++));
-            merged.append(word2.charAt(j++));
-
-        }
-        while(i < word1.length()){
-            merged.append(word1.charAt(i++));
-            }
-        while(j < word2.length()){
-            merged.append(word2.charAt(j++));
-        }
-
-        return merged.toString();
-    }
-}
+// Last updated: 1/17/2026, 3:57:38 PM
+1class Solution {
+2    public String mergeAlternately(String word1, String word2) {
+3        StringBuilder merged = new StringBuilder();
+4        int i = 0;
+5        int j = 0;
+6
+7        while(i < word1.length() && j < word2.length()){
+8            merged.append(word1.charAt(i++));
+9            merged.append(word2.charAt(j++));
+10
+11        }
+12        while(i < word1.length()){
+13            merged.append(word1.charAt(i++));
+14            }
+15        while(j < word2.length()){
+16            merged.append(word2.charAt(j++));
+17        }
+18
+19        return merged.toString();
+20    }
+21}
