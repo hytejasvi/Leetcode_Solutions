66ab22fb1196baef329db6e9fd3431477449f970
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-17 10:04:55 +0100

    File updated at 1/17/2026, 10:04:55 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.java
@@ -1,20 +1,22 @@
-// Last updated: 9/23/2025, 3:47:13 PM
-class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] res = new int[2];
-        int left = 0, right = numbers.length-1;
-        while (left < right) {
-            int num = numbers[left] + numbers[right];
-            if(num == target) {
-                res[0] = left+1;
-                res[1] = right+1;
-                return res;
-            } else if(num > target) {
-                right --;
-            } else {
-                left ++;
-            }
-        }
-        return res;
-    }
-}
+// Last updated: 1/17/2026, 10:04:55 AM
+1class Solution {
+2    public int[] twoSum(int[] numbers, int target) {
+3        int[] res = new int[2];
+4        int left = 0, right = numbers.length-1;
+5        while (left < right) {
+6            int num = numbers[left] + numbers[right];
+7            if(num == target) {
+8                res[0] = left+1;
+9                res[1] = right+1;
+10                return res;
+11            } else if(num > target) {
+12                right --;
+13            } else {
+14                left ++;
+15            }
+16        }
+17        return res;
+18    }
+19    //TC: O(n) -> we get the result in a single pass
+20    //SC: O(1) -> we only use certain variables and no additional space
+21}
