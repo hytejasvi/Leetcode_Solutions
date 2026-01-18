2ff1bd20ddd124323bcb2015075fb2d34a25edcd
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-18 12:17:14 +0100

    File updated at 1/18/2026, 12:17:14 PM

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,14 +1,14 @@
-// Last updated: 11/21/2025, 10:09:30 AM
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        if(nums.length == 0) return 0;
-        int i = 0;
-        for(int j=0; j<=nums.length-1; j++){
-            if(nums[j]!=val){
-                nums[i]=nums[j];
-                 i++;
-            }
-        }
-        return i;
-    }
-}
+// Last updated: 1/18/2026, 12:17:14 PM
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        if(nums.length == 0) return 0;
+4        int left = 0, right = 0;
+5        while(right < nums.length) {
+6            if(nums[right] != val) {
+7                nums[left++] = nums[right];
+8            }
+9            right++;
+10        }
+11        return left;
+12    }
+13}
