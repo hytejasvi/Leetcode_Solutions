813779310cc8c9a23e11e6d7b6944edc729cdeec
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-18 12:35:14 +0100

    File updated at 1/18/2026, 12:35:15 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,31 +1,31 @@
-// Last updated: 9/12/2025, 10:33:23 PM
-class Solution {
-    public int majorityElement(int[] nums) {
-        /*Map<Integer, Integer> myMap = new HashMap<>();
-        int n = nums.length/2;        
-        for(int num: nums) {
-            myMap.put(num, myMap.getOrDefault(num, 0)+1);
-            if (myMap.get(num) > n) {
-            return num;
-        }
-        }
-        return n;*/
-
-        // a single pass with no extra memory:
-        int num = 0;
-        int count = 0;
-        int n = nums.length / 2;
-        for(int a: nums) {
-            if(count == 0) {
-                num = a;
-            }
-            //count += num==a ? 1: -1; short way of if-else
-            if(num ==a) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-        return num;
-    }
-}
+// Last updated: 1/18/2026, 12:35:15 PM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        /*Map<Integer, Integer> myMap = new HashMap<>();
+4        int n = nums.length/2;        
+5        for(int num: nums) {
+6            myMap.put(num, myMap.getOrDefault(num, 0)+1);
+7            if (myMap.get(num) > n) {
+8            return num;
+9        }
+10        }
+11        return n;*/
+12
+13        // a single pass with no extra memory:
+14        int num = 0;
+15        int count = 0;
+16        int n = nums.length / 2;
+17        for(int a: nums) {
+18            if(count == 0) {
+19                num = a;
+20            }
+21            //count += num==a ? 1: -1; short way of if-else
+22            if(num ==a) {
+23                count++;
+24            } else {
+25                count--;
+26            }
+27        }
+28        return num;
+29    }
+30}
