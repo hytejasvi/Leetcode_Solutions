38d7bf4ad9f424fef56625ee8188e847ad8f7ef8
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-20 22:25:47 +0100

    File updated at 1/20/2026, 10:25:48 PM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,4 +1,4 @@
-// Last updated: 1/20/2026, 10:24:36 PM
+// Last updated: 1/20/2026, 10:25:48 PM
 1class Solution {
 2    public List<List<Integer>> fourSum(int[] nums, int target) {
 3        Arrays.sort(nums);
@@ -10,29 +10,30 @@
 9            for(int j = i+1; j < nums.length-2;j++) {
 10
 11                if(j > i+1 && nums[j] == nums[j-1]) {
-12                    continue;
-13                }
-14                int left = j+1, right = nums.length-1;
-15                while( left < right) {
-16                    long sum = (long) nums[i]+nums[j]+nums[left]+nums[right];
-17                    if(sum > target) {
-18                        right--;
-19                    } else if(sum < target) {
-20                        left++;
-21                    } else {
-22                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-23                        while(left < right && nums[left] == nums[left+1]) {
-24                            left++;
-25                        }
-26                        while(left < right && nums[right] == nums[right-1]) {
-27                            right--;
-28                        }
-29                        left++;
-30                        right--;
-31                    }
-32                }
-33            }
-34        }
-35        return res;
-36    }
-37}
+12                    //here checking j > i+1 so we only check for duplicates if j has moved any positions in teh current iteration.
+13                    continue;
+14                }
+15                int left = j+1, right = nums.length-1;
+16                while( left < right) {
+17                    long sum = (long) nums[i]+nums[j]+nums[left]+nums[right];
+18                    if(sum > target) {
+19                        right--;
+20                    } else if(sum < target) {
+21                        left++;
+22                    } else {
+23                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+24                        while(left < right && nums[left] == nums[left+1]) {
+25                            left++;
+26                        }
+27                        while(left < right && nums[right] == nums[right-1]) {
+28                            right--;
+29                        }
+30                        left++;
+31                        right--;
+32                    }
+33                }
+34            }
+35        }
+36        return res;
+37    }
+38}
