777effc37c1288c779fec9183e4bb6b5b3cea80e
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-21 22:13:55 +0100

    File updated at 1/21/2026, 10:13:55 PM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
@@ -1,21 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-}
+// Last updated: 1/21/2026, 10:13:55 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode middleNode(ListNode head) {
+13        ListNode fast = head;
+14        while(fast != null && fast.next != null) {
+15            head = head.next;
+16            fast = fast.next.next;
+17        }
+18        return head;
+19    }
+20}
