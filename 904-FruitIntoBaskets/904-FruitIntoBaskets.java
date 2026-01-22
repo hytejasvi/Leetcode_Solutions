6e8d291ff1e893b85efd3ed76ab2730ac48859a7
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-22 12:28:30 +0100

    File updated at 1/22/2026, 12:28:30 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.java
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -1,21 +1,21 @@
-// Last updated: 10/5/2025, 12:27:12 PM
-class Solution {
-    public int totalFruit(int[] fruits) {
-        Map<Integer, Integer> mm = new HashMap<>();
-        int left = 0;
-        int res = 0;
-        for (int i=0;i < fruits.length;i++){
-            mm.put(fruits[i], mm.getOrDefault(fruits[i], 0)+1);
-            
-            while(mm.size() > 2) {
-                mm.put(fruits[left], mm.get(fruits[left]) - 1);
-                if (mm.get(fruits[left]) == 0) {
-                    mm.remove(fruits[left]);
-                }
-                left++;
-            }
-            res = Math.max(res, i - left+1);
-        }
-        return res;
-    }
-}
+// Last updated: 1/22/2026, 12:28:30 PM
+1class Solution {
+2    public int totalFruit(int[] fruits) {
+3        Map<Integer, Integer> mm = new HashMap<>();
+4        int left = 0;
+5        int res = 0;
+6        for (int i=0;i < fruits.length;i++){
+7            mm.put(fruits[i], mm.getOrDefault(fruits[i], 0)+1);
+8            
+9            while(mm.size() > 2) {
+10                mm.put(fruits[left], mm.get(fruits[left]) - 1);
+11                if (mm.get(fruits[left]) == 0) {
+12                    mm.remove(fruits[left]);
+13                }
+14                left++;
+15            }
+16            res = Math.max(res, i - left+1);
+17        }
+18        return res;
+19    }
+20}
