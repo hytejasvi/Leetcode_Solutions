00c8104b5e47af6a4f048355c7d74f495a9823a2
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2026-01-22 10:05:58 +0100

    File updated at 1/22/2026, 10:05:58 AM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
@@ -1,18 +1,16 @@
-// Last updated: 11/25/2025, 11:15:19 AM
-class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        int left = 0;
-        int minLength = Integer.MAX_VALUE;
-        int sum = 0;
-        int right = 0;
-        for(int i=0;i < nums.length;i++) {
-            sum = sum+nums[i];
-            while(sum >= target) {
-                minLength = Math.min(minLength, i-left+1);
-                sum = sum-nums[left];
-                left++;
-            }
-        }
-        return minLength == Integer.MAX_VALUE ? 0: minLength;
-    }
-}
+// Last updated: 1/22/2026, 10:05:58 AM
+1class Solution {
+2    public int minSubArrayLen(int target, int[] nums) {
+3        int minSubArray = Integer.MAX_VALUE;
+4        int left = 0, windowSum = 0;
+5        for(int i=0;i< nums.length;i++) {
+6            windowSum += nums[i];
+7            while(windowSum >= target) {
+8                minSubArray = Math.min(i-left+1, minSubArray);
+9                windowSum -= nums[left];
+10                left++;
+11            }
+12        }
+13        return minSubArray == Integer.MAX_VALUE ? 0 : minSubArray;
+14    }
+15}
